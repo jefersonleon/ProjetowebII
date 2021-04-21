bd11f0b029ef2b3c1fc8b9bf139fbf118300afe0
Jeferson Leon <jefersonleonblue@gmail.com>
2021-04-20 21:20:50 -0300

    Conexão com o banco de dados

---
+++ includes/banco.php
@@ -0,0 +1,32 @@
+<?php
+   $servername = "localhost";
+   $username = "root";
+   $password = "";
+   $data = "bdgames";
+   $port = "3307";
+
+   $banco = new mysqli($servername, $username,$password,$data,$port);//objeto de conexão
+
+   if($banco->connect_errno){
+       echo "<p> Encontrei um erro $banco->errno --> $banco->connect_error </p>";
+       die();
+   }
+
+     $banco->query("SET NAMES 'utf8");
+     $banco->query("SET character_set_connection=utf8");
+     $banco->query("SET character_set_cliente=utf8");
+     $banco->query("SET character_set_results=utf8");
+
+/*
+   $busca = $banco->query("select * from tbgeneros order by genero");
+   if(!$busca){
+       echo"<p. Falha na busca! $banco->error</p>";
+   }else{
+    echo" Lista de Generos<br>";
+       while($reg = $busca->fetch_object())
+       {
+         echo" $reg->genero <br>";
+       }
+
+    }
+*/
